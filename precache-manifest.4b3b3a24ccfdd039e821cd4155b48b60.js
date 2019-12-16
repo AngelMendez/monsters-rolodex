d6a392a0c220948c2da10b3540712f6afd9bcef5
Angel Mendez <hello@finangraphy.com>
2019-12-16 17:08:36 -0600

    Updates

---
+++ precache-manifest.4b3b3a24ccfdd039e821cd4155b48b60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23adeb734cec8387219a407848915a0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b73a6c40e1936fa554cb",
+    "url": "/monsters-rolodex/static/css/main.33752246.chunk.css"
+  },
+  {
+    "revision": "1b43d2eaa999d2a50f77",
+    "url": "/monsters-rolodex/static/js/2.c698f0ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.c698f0ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b73a6c40e1936fa554cb",
+    "url": "/monsters-rolodex/static/js/main.80a716c7.chunk.js"
+  },
+  {
+    "revision": "366f143aac14b6e58293",
+    "url": "/monsters-rolodex/static/js/runtime-main.1427f30c.js"
+  }
+]);
